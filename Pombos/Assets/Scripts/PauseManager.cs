14b8dab4c75882ec00847dc2dea852dee97e6462
Gabriel Gomes <ggpereira@inf.ufsm.br>
2023-05-09 21:32:17 -0300

    Add menu de pause

--- Pombos/Assets/Scripts/PauseManager.cs
+++ Pombos/Assets/Scripts/PauseManager.cs
@@ -6,34 +6,37 @@ using UnityEngine;
 public class PauseManager : MonoBehaviour
 {   
     private bool gameIsPaused = false;
-     private void Start() {
-        VisualElement root = GetComponent<UIDocument>().rootVisualElement;
-        root.style.display = DisplayStyle.None;
+    public UIDocument uIDocument;
+
+    private void ShowMenu() {
+        uIDocument.gameObject.SetActive(true);
+        this.PauseGame();
+        this.gameIsPaused = true;
+    }
+
+    private void HideMenu() {
+        uIDocument.gameObject.SetActive(false);
+        this.UnPauseGame();
+        this.gameIsPaused = false;
     }
 
     public void PauseGame()
     {
-        VisualElement root = GetComponent<UIDocument>().rootVisualElement;
         Time.timeScale = 0;
-        root.style.display = DisplayStyle.Flex;
-        this.gameIsPaused = true;
     }
 
     public void UnPauseGame()
     {
-        VisualElement root = GetComponent<UIDocument>().rootVisualElement;
         Time.timeScale = 1;
-        root.style.display = DisplayStyle.None;
-        this.gameIsPaused =  false;
     }
 
     public void Update() {
         if(Input.GetKeyDown("escape")) {
-            if(gameIsPaused) {
-                UnPauseGame();
+            if(this.gameIsPaused) {
+                this.HideMenu();
                 return;
             } 
-            PauseGame();
+            this.ShowMenu();
         }
     }
 }
