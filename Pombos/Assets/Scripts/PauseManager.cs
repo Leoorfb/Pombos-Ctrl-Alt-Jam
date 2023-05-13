68d1861118043e56f3167d3929671e34966f61d2
Gabriel Gomes <ggpereira@inf.ufsm.br>
2023-05-13 17:50:11 -0300

    Add menu de pause melhorado

--- Pombos/Assets/Scripts/PauseManager.cs
+++ Pombos/Assets/Scripts/PauseManager.cs
@@ -27,17 +27,32 @@ public class PauseManager : MonoBehaviour
         WeaponBase weapon = player.GetComponent<WeaponsManager>().GetWeaponBase();
         int playerGold = player.GetComponent<PlayerGold>().gold;
 
+
         Label armorLabelUI = uiDocument.rootVisualElement.Q<Label>("ArmorLabel");
         armorLabelUI.text = armorLabelUI.text.Replace("XX", player.armor.ToString());
 
         Label healthLabelUI = uiDocument.rootVisualElement.Q<Label>("HealthLabel");
-        healthLabelUI.text = healthLabelUI.text.Replace("XX", player.maxHp.ToString());
+        healthLabelUI.text = healthLabelUI.text.Replace("XX", player.hp.ToString() + "/"  + player.maxHp.ToString());
 
         Label damageLabelUI = uiDocument.rootVisualElement.Q<Label>("DamageLabel");
         damageLabelUI.text = damageLabelUI.text.Replace("XX", weapon.weaponStats.damage.ToString()); 
 
         Label coinLabelUI = uiDocument.rootVisualElement.Q<Label>("CoinLabel");
         coinLabelUI.text = coinLabelUI.text.Replace("XX", playerGold.ToString());
+
+        Label speedLabelUI = uiDocument.rootVisualElement.Q<Label>("SpeedLabel");
+        speedLabelUI.text = speedLabelUI.text.Replace("XX", player.speed.ToString());
+
+        Label shootingSpeedLabelUI = uiDocument.rootVisualElement.Q<Label>("ShootingSpeedLabel");
+        shootingSpeedLabelUI.text = shootingSpeedLabelUI.text.Replace("XX", weapon.weaponStats.fireRate.ToString());
+
+        float normalizedCriticalValue = player.critChance * 100;
+
+        Label criticalLabelUI = uiDocument.rootVisualElement.Q<Label>("CriticalLabel");
+        criticalLabelUI.text = criticalLabelUI.text.Replace("XX", normalizedCriticalValue.ToString() + "%");
+
+        Label ammoMaxLabelUI = uiDocument.rootVisualElement.Q<Label>("AmmoMaxLabel");
+        ammoMaxLabelUI.text = ammoMaxLabelUI.text.Replace("XX", weapon.weaponStats.ammoMax.ToString());
 
         Button closeStatsBtn = uiDocument.rootVisualElement.Q<Button>("CloseStatsBtn");
         closeStatsBtn.clicked += () => this.HideMenu();
